c26d75bb3ca7fa72cae58b2bbeab8c711e771af7
Jordan Coult <jordan@auterion.com>
2025-02-04 22:02:56 +0000

    Replace hardcoded value with variable

--- tools/decodeCrashDump.sh
+++ tools/decodeCrashDump.sh
@@ -9,7 +9,7 @@ MODULENAME="QGC-Gov" # Generic name
 $TOOLSDIR/minidump_stackwalk -s -c "$DUMPFILE" "$SYMSDIR" 2>/dev/null > "$OUTPUTFILE"
 
 if grep -q "No symbols, .*$MODULENAME.*" "$OUTPUTFILE"; then
-    MODULEID=$(grep -P "No symbols, .*QGC-Gov.*" "$OUTPUTFILE" | grep -Po "[0-9A-F]{33}")
+    MODULEID=$(grep -P "No symbols, .*$MODULENAME.*" "$OUTPUTFILE" | grep -Po "[0-9A-F]{33}")
     echo "WARNING: No debug symbols found for $MODULENAME with module-id $MODULEID"
 fi
 
